36700a108b99324e8db81ef1e25111fcb02ff0c0
EdwardBaker <edward.baker@nhm.ac.uk>
2014-03-14 17:50:58 +0000

    Safe .local names

--- osdl.h
+++ osdl.h
@@ -1,6 +1,40 @@
-struct  sensor_t {
- int id;
- char name[50];
- char url[50]; 
+#define MAX_STRING 60
+
+char stringBuffer[MAX_STRING];
+
+struct  OSDLsensor {
+  char id[10];
+  char name[50];
+  char data_url[50]; 
+  char request_path[10];
 };
+
+struct OSDLaction {
+  int id;
+  char name[50];
+  char url[50];
+  char path[10];
+};
+
+char* OSDLgetString(const char* str) {
+  strcpy_P(stringBuffer, (char*)str);
+  return stringBuffer;
+}
+
+char* OSDLsafeDeviceName(char* str) {
+  strcpy(stringBuffer, (char*)str);
+  for (int i=0; i < MAX_STRING; i++) {
+   if (stringBuffer[i] == '\0') {
+     break;
+   }
+   if (stringBuffer[i] == '.') {
+     stringBuffer[i] = '_';
+   } 
+   if (stringBuffer[i] == ' ') {
+     stringBuffer[i] = '-';
+   }
+  }
+  return stringBuffer;
+}
+
 
--- plugnplay.ino
+++ plugnplay.ino
@@ -1,85 +1,79 @@
 
-#if defined(ARDUINO) && ARDUINO > 18
 #include <SPI.h>
-#endif
 #include <Ethernet.h>
 #include <EthernetBonjour.h>
-#include "sensor.h"
+#include "osdl.h"
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
-const PROGMEM sensor_t sensors[] = {
-  {1, "Ultrasupersonic Sensor._sensor", "http://osdl.myspecies.info/node/4"}, 
-  {2, "InfraRed Sensor._sensor", "http://osdl.myspecies.info/node/5"}
+const PROGMEM char device_name[] = "EdSensor";
+const PROGMEM OSDLsensor sensors[] = {
+  {"1", "Ultrasupersonic Sensor", "http://osdl.myspecies.info/node/4", "/s/1"}, 
+  {"2", "InfraRed Sensor", "http://osdl.myspecies.info/node/5", "/s/2"}
+};
+const PROGMEM OSDLaction actions[] = {
+  {1, "Take a photo", "http://gooogle.com/takeaphoto", "/a/1"}
 };
 
-const int c_sensors = sizeof(sensors) / sizeof(sensor_t);
+const int c_sensors = sizeof(sensors) / sizeof(OSDLsensor);
+const int c_actions = sizeof(actions) / sizeof(OSDLaction);
 
 EthernetServer server(80);
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   Ethernet.begin(mac);
   server.begin();
-  EthernetBonjour.begin("arduino");
-  for (int i = 0; i < c_sensors -1; i++) {
-    Serial.print("Name: ");
-    Serial.println(sensors[i].name);
-    //EthernetBonjour.addServiceRecord(getString(sensors[i].name), 80, MDNSServiceTCP, "\x7path=/2");
-  }
+  EthernetBonjour.begin(OSDLsafeDeviceName(OSDLgetString(device_name)));
+  EthernetBonjour.addServiceRecord(
+    strcat(OSDLsafeDeviceName(OSDLgetString(device_name)), "._sensor"), 
+    80, 
+    MDNSServiceTCP
+    //"\xdpath=/osdl.js"
+  );
 }
 
-void loop()
-{ 
-  // This actually runs the Bonjour module. YOU HAVE TO CALL THIS PERIODICALLY,
-  // OR NOTHING WILL WORK! Preferably, call it once per loop().
+void loop() { 
   EthernetBonjour.run();
-
-  // The code below is just taken from the "WebServer" exmple in the Ethernet
-  // library. The only difference here is that this web server gets announced
-  // over Bonjour, but this happens in setup(). This just displays something
-  // in the browser when you connect.
   EthernetClient client = server.available();
   if (client) {
-    //Serial.println("new client");
-    // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        //Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
         if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-	  client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println("Connection: close");
+	  client.println("Refresh: 5");
           client.println();
-          client.print("{\"sensor\":[{\"name\":\"");
-          //client.print(getString("sensor_name"));
-          client.print("\", \"data_url\":\"");
-          //client.print(getString("sensor_data_url"));
-          client.println("\"} ]}");
+          client.print("{\"sensors\":[");
+            for (int i = 0; i < c_sensors; i++) {
+              client.print("{\"");
+              client.print(OSDLgetString(sensors[i].id));
+                client.print("\":[");
+                  client.print("{\"name\":\""); client.print(OSDLgetString(sensors[i].name)); client.print("\"},");
+                  client.print("{\"data_url\":\""); client.print(OSDLgetString(sensors[i].data_url)); client.print("\"},");
+                  client.print("{\"request_path\":\""); client.print(OSDLgetString(sensors[i].request_path)); client.print("\"}");
+                client.print("]");
+             client.print("}");
+             if (i != c_sensors - 1) {
+               client.print(",");
+             }
+            }
+          client.println("]}");
           break;
         }
         if (c == '\n') {
-          // you're starting a new line
           currentLineIsBlank = true;
         } 
         else if (c != '\r') {
-          // you've gotten a character on the current line
           currentLineIsBlank = false;
         }
       }
     }
     // give the web browser time to receive the data
     delay(1);
-    // close the connection:
     client.stop();
-    //Serial.println("client disonnected");
   }
 }
