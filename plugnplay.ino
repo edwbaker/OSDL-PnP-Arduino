beba79f46a81d30f9206f27f42febf0db985a2bf
Ed Baker <edwbaker@gmail.com>
2015-03-30 19:58:30 +0100

    Reset page and path each iteration

--- plugnplay.ino
+++ plugnplay.ino
@@ -2,27 +2,23 @@
 #include <Ethernet.h>
 #include <EthernetBonjour.h>
 #include "osdl.h"
-byte mac[] = { 
+byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 const PROGMEM char device_name[] = "EdSensor";
 const PROGMEM OSDLsensor sensors[] = {
   {
-    "1", "Arduino value of millis()", "http://osdl.myspecies.info/node/7", "/s/1"  }
+    "1", "Arduino value of millis()", "http://osdl.myspecies.info/node/7", "/s/1"   }
   ,
   {
-    "2", "A random number", "http://osdl.myspecies.info/node/8", "/s/2"  }
+    "2", "A random number", "http://osdl.myspecies.info/node/8", "/s/2"   }
 };
 const PROGMEM OSDLaction actions[] = {
   {
-    1, "Take a photo", "http://gooogle.com/takeaphoto", "/a/1"  }
+    1, "Take a photo", "http://gooogle.com/takeaphoto", "/a/1"   }
 };
 const int c_sensors = sizeof(sensors) / sizeof(OSDLsensor);
 const int c_actions = sizeof(actions) / sizeof(OSDLaction);
 EthernetServer server(80);
-
-String path = "";
-String page = "";
-
 
 void setup() {
   Serial.begin(9600);
@@ -32,26 +28,26 @@ void setup() {
   EthernetBonjour.addServiceRecord(
   strcat(OSDLsafeDeviceName(OSDLgetString(device_name)), "._sensor"),
   80,
-  MDNSServiceTCP
-    //"\xdpath=/osdl.js"
-  );
+  MDNSServiceTCP,
+  "\xcpath=osdl.js"
+    );
 }
 void loop() {
   EthernetBonjour.run();
   EthernetClient client = server.available();
   if (client) {
     boolean currentLineIsBlank = true;
+    String path = "";
+    String page = "";
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
         path += c;
-
         if (c == '\n') {
           if (path.startsWith("GET ")) {
             page = path.substring(5, path.lastIndexOf("HTTP") - 1);
           }
         }
-
         if (c == '\n' && currentLineIsBlank) {
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
@@ -63,14 +59,14 @@ void loop() {
               client.print("{\"");
               client.print(OSDLgetString(sensors[i].id));
               client.print("\":[");
-              client.print("{\"name\":\""); 
-              client.print(OSDLgetString(sensors[i].name)); 
+              client.print("{\"name\":\"");
+              client.print(OSDLgetString(sensors[i].name));
               client.print("\"},");
-              client.print("{\"data_url\":\""); 
-              client.print(OSDLgetString(sensors[i].data_url)); 
+              client.print("{\"data_url\":\"");
+              client.print(OSDLgetString(sensors[i].data_url));
               client.print("\"},");
-              client.print("{\"request_path\":\""); 
-              client.print(OSDLgetString(sensors[i].request_path)); 
+              client.print("{\"request_path\":\"");
+              client.print(OSDLgetString(sensors[i].request_path));
               client.print("\"}");
               client.print("]");
               client.print("}");
@@ -105,5 +101,4 @@ void loop() {
     delay(1);
     client.stop();
   }
-}
 }
